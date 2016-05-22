cab0c716a8320711da2b16299a56a40ecef53c2a
Cesar Hernandez Antonio <hernandez.antonio.cesar19@gmail.com>
2016-05-22 00:53:05 -0500

    Primer commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # accesslogic-rfid
-Controlador del lector RFID RC522
+Controlador del lector RFID RC522 para Raspberry Pi 2 para el proyecto ACCESS LOGIC.
+
+Este controlador esta basado en el siguiente repositorio: [MFRC522-python](https://github.com/mxgxw/MFRC522-python).
+
+## Plugins necesarios
+Para ejecutar este proyecto es necesario contar con los siguientes plugins instalados en la Raspberry Pi:
+
+* RPi.GPIO
+* SPI
+* PyMongo
+* Adafruit_CharLCD
+
+## Hardware necesario
+Durante la realizacion de este proyecto se hace uso del siguiente hardware:
+
+* Raspberry Pi 2 Model B
+* Lector RFID RC522
+* Tarjetas NFC Mifare Classic 1k Card
+* LCD 16x2
---
+++ rfid.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+import Adafruit_CharLCD as LCD
+import time
+
+
+continue_reading = True
+
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read():
+    global continue_reading
+    print "Ctrl+C capturado, Finalizando lectura."
+    continue_reading = False
+    GPIO.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+# Configure the GPIOs of the LCD are connected
+lcd = LCD.Adafruit_CharLCD(rs=26, en=19, d4=13, d5=6, d6=5, d7=27, cols=16, lines=2)
+
+# Clear de screen
+lcd.clear()
+
+# Welcome message
+print "Bienvenido al ejemplo de lectura de tarjetas NFC"
+print("Presiona Ctrl-C para cerrar")
+
+time.sleep(1)
+
+print "Listo!"
+
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+
+    # Scan for cards
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # If a card is found
+    if status == MIFAREReader.MI_OK:
+        print "Tarjeta detectada"
+
+    # Get the UID of the card
+    (status, uid) = MIFAREReader.MFRC522_Anticoll()
+
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
+
+        # Print UID in the LCD
+        lcd.message("Card read UID:\n" + str(uid[0]) + "," + str(uid[1]) + ","+str(uid[2]) + "," + str(uid[3]))
+
+        # This is the default key for authentication
+        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+        # Select the scanned tag
+        MIFAREReader.MFRC522_SelectTag(uid)
+
+        # Authenticate
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+        # Check if authenticated
+        if status == MIFAREReader.MI_OK:
+            MIFAREReader.MFRC522_Read(8)
+            MIFAREReader.MFRC522_StopCrypto1()
+        else:
+            print "Authentication error"
+
+        # Clear de screen
+        time.sleep(2)
+        lcd.clear()
